5550432ab3f4c6471ea1362b6086432240f36f7a
Andrey <borohovandrey@gmail.com>
2022-09-18 16:03:08 +0300

    init commit

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
-    let helloMessage = 'Hello Incubator`'
+    let helloMessage = 'Hello Incubator?'
     res.send(helloMessage)
 })
 
